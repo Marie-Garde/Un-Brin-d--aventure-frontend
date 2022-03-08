baf967f81308bd3cbea78e7bbb9c063cf4440a07
Marie-Garde <marie_garde@ymail.com>
2022-03-08 09:06:51 +0100

    add first files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 
-const routes: Routes = [];
+import { FrontPageComponent } from '../components/front-page/front-page.component';
+import { LoginComponent } from '../components/login/login.component';
+import { CustomersComponent } from '../components/customers/customers.component';
+
+const routes: Routes = [
+  { path: '', component: FrontPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'customers', component: CustomersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
