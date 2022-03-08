8ef82ace47615bd366cdd90e88363ff101e6a710
Marie-Garde <marie_garde@ymail.com>
2022-03-08 13:39:15 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  ChevronDownModule,
+} from '@carbon/icons-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontPageComponent } from '../components/front-page/front-page.component';
+import { HeaderComponent } from '../components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontPageComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChevronDownModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/components/front-page/front-page.component.ts
+++ src/components/front-page/front-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-front-page',
@@ -8,15 +7,11 @@ import { Router } from '@angular/router';
 })
 export class FrontPageComponent implements OnInit {
 
-  constructor(
-    private router: Router,
-  ) { }
+  constructor( ) { }
 
   ngOnInit(): void {
   }
 
-  public redirectToManageAccount() {
-    this.router.navigate(['/auth']);
-  }
+
 
 }
