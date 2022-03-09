ce6a30365241a5f4b03b945d930bba18a56c770c
Marie-Garde <marie_garde@ymail.com>
2022-03-09 09:29:09 +0100

    footer ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {
   ChevronDownModule,
+  LogoFacebookModule,
+  LogoInstagramModule,
 } from '@carbon/icons-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontPageComponent } from '../components/front-page/front-page.component';
 import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontPageComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ChevronDownModule,
+    LogoFacebookModule,
+    LogoInstagramModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
